2f48ad5282845381e4d75c3931b720e8a683f2c2
guiasis <57735271+guiasis@users.noreply.github.com>
2022-09-22 13:47:16 -0300

    feature: first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <editor />
+</template>
+
+<script>
+import Editor from './components/Editor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Editor
+  }
+}
+</script>
+
+<style>
+body {
+  background: #eee;
+  padding: 10px;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+</style>
